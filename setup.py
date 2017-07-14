a7814debe52d3d41a4201084a290011f78663906
azogue <eugenio.panadero@gmail.com>
2017-07-14 14:09:45 +0200

    include long description

--- setup.py
+++ setup.py
@@ -3,17 +3,24 @@
 A python library to make psychrometric charts and overlay information in them.
 
 """
+import os
 from setuptools import setup, find_packages
+
 from psychrochart import __version__ as version
 
 
 packages = find_packages(exclude=['docs', '*tests*', 'notebooks', 'htmlcov'])
+
+basedir = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(basedir, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='psychrochart',
     version=version,
     description='A Python 3 library to make psychrometric charts and overlay '
                 'information on them.',
+    long_description=long_description,
     keywords='psychrometrics, moist, humid air, climate control, matplotlib',
     author='Eugenio Panadero',
     author_email='eugenio.panadero@gmail.com',
